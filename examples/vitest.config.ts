34c757aa2755c4a7ead2454cb7b8ac011cc04008
Zenichi Amano <zenichi.amano@gahojin.co.jp>
2026-03-19 15:00:28 +0900

    build(deps): 依存ライブラリアップデート
    - vite 7.3.1 -> 8.0.1
    - vitest 4.0.18 -> 4.1.0
    - vitest-browser-react 2.0.5 -> 2.1.0
    - vitest/browser-playwright 4.0.10 -> 4.1.0
    - vitejs/plugin-react 5.1.4 -> 6.0.1
    - rolldown 1.0.0-rc.8 -> 1.0.0-rc.10
    - terser 5.46.0 -> 5.46.1
    - turbo 2.8.16 -> 2.8.19
    - pnpm 10.30.3 -> 10.32.1
    
    Signed-off-by: Zenichi Amano <zenichi.amano@gahojin.co.jp>

--- examples/vitest.config.ts
+++ examples/vitest.config.ts
@@ -1,13 +1,15 @@
 import react from '@vitejs/plugin-react'
 import { playwright } from '@vitest/browser-playwright'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   define: {
     'process.env.VITE_SITE_KEY': JSON.stringify('dummy'),
     global: {},
+  },
+  resolve: {
+    tsconfigPaths: true,
   },
   test: {
     globals: true,
