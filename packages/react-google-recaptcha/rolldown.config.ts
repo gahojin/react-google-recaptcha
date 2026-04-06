712bbbf039647fb297631d705104a0e577fc1004
Zenichi Amano <zenichi.amano@gahojin.co.jp>
2026-04-06 18:17:30 +0900

    fix: dtsファイル生成に、rolldown-plugin-dtsを使用する
    Signed-off-by: Zenichi Amano <zenichi.amano@gahojin.co.jp>

--- packages/react-google-recaptcha/rolldown.config.ts
+++ packages/react-google-recaptcha/rolldown.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'rolldown'
-import IsolatedDecl from 'unplugin-isolated-decl/rolldown'
+import { dts } from 'rolldown-plugin-dts'
 
 export default defineConfig([
   {
     external: [/^node:/, /^@types\//, /^@xstate\//, /^react\//, 'react', 'react-dom', 'xstate'],
     treeshake: true,
     input: 'src/index.ts',
-    output: [{ dir: 'dist', format: 'esm', entryFileNames: '[name].mjs', sourcemap: true, cleanDir: true }],
-    plugins: [IsolatedDecl()],
+    output: [{ dir: 'dist', format: 'es', sourcemap: true, cleanDir: true }],
+    plugins: [dts()],
   },
 ])
